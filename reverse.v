725720d859cb42740bdfe2facf97b698f58002db
Govind Singh <61302637+adiboy17@users.noreply.github.com>
2022-02-22 23:56:11 +0530

    init code

---
+++ reverse.v
@@ -0,0 +1,29 @@
+//reverse function arr[] start to end
+void reverseArray(int arr[], int start, int end){
+    int temp;
+    while(start < end){
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size){
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d", arr[i]);
+        
+    printf("\n");
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    int n = sizeof(arr);
+    printArray(arr, n);
+    reverseArray(arr, 0, n-1);
+    printf("Reversed array is \n");
+    printArray(arr, n);
+    return 0;
+}
